b2e1cb9908d0ca33f81b308d1d556113e8d6d322
Alberto <129074553+xalbertho@users.noreply.github.com>
2025-02-09 09:06:12 -0600

    Update spotify.py
    cambio en el tema (obscuro)

--- api/spotify.py
+++ api/spotify.py
@@ -21,7 +21,7 @@ SPOTIFY_SECRET_ID = os.getenv("SPOTIFY_SECRET_ID")
 SPOTIFY_REFRESH_TOKEN = os.getenv("SPOTIFY_REFRESH_TOKEN")
 SPOTIFY_TOKEN = ""
 
-FALLBACK_THEME = "spotify.html.j2"
+FALLBACK_THEME = "spotify-dark.html.j2"
 
 REFRESH_TOKEN_URL = "https://accounts.spotify.com/api/token"
 NOW_PLAYING_URL = "https://api.spotify.com/v1/me/player/currently-playing"
